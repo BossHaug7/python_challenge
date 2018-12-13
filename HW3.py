c75556cf9dfca4acc777a223e92cfd77bebce34d
Andrew Haug <44251652+BossHaug7@users.noreply.github.com>
2018-12-12 23:29:26 -0700

    Delete HW3.py

--- HW3.py
+++
@@ -1,8 +0,0 @@
-import csv
-file_name = "03-Python_Homework_PyBank_Resources_budget_data.csv"
-with open(file_name) as myfile:
-    csvreader = csv.reader(myfile)
-
-    for row in csvreader: 
-        print(row)
-
